aa01e9bb0c9771323e0477325bb4a2f6bde909ba
NiranDx <niran3688@gmail.com>
2019-01-28 02:59:31 +0700

    Up program

---
+++ cal.js
@@ -0,0 +1,4 @@
+exports =  calvolume = (x,y) => {
+     var pir2 = Math.PI * y * y
+     return (pir2 * x) / 3
+}
---
+++ hw2.js
@@ -0,0 +1,7 @@
+require('./cal.js')
+
+var height = 5;
+var radius = 3;
+var volume = calvolume(height,radius)
+
+console.log("this is Volume:"+volume);
